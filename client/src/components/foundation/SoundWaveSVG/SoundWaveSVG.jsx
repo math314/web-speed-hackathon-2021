11ccb266f65fa849051411c9f290abd8409b566f
314@live.jp <__math>
2021-12-15 02:51:05 +0900

    change order

--- client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
+++ client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import React from 'react';
 
+// async function waitForNextAnimationFrame() {
+//   new Promise((resolve, reject) => {
+//     window.requestAnimationFrame(() => resolve());
+//   });
+// }
+
 /**
  * @param {ArrayBuffer} data
  * @returns {Promise<{ max: number, peaks: number[] }}
  */
 async function calculate(data) {
+  // await waitForNextAnimationFrame();
   const audioCtx = new AudioContext();
 
   // 音声をデコードする
@@ -14,19 +21,21 @@ async function calculate(data) {
     audioCtx.decodeAudioData(data.slice(0), resolve, reject);
   });
   // 左の音声データの絶対値を取る
-  console.log(buffer.getChannelData(0));
+  // console.log(buffer.getChannelData(0));
   const leftData = _.map(buffer.getChannelData(0), Math.abs);
-  console.log(buffer.getChannelData(1));
+  // console.log(buffer.getChannelData(1));
   // 右の音声データの絶対値を取る
   const rightData = _.map(buffer.getChannelData(1), Math.abs);
 
   // 左右の音声データの平均を取る
-  const normalized = _.map(_.zip(leftData, rightData), _.mean);
-  // 100 個の chunk に分ける
-  const chunks = _.chunk(normalized, Math.ceil(normalized.length / 100));
-  // chunk ごとに平均を取る
-  const peaks = _.map(chunks, _.mean);
-  // chunk の平均の中から最大値を取る
+  const calcPeaks = (data) => {
+    const chunks = _.chunk(data, Math.ceil(data.length / 100));
+    return _.map(chunks, _.mean);
+  };
+  const leftPeaks = calcPeaks(leftData);
+  const rightPeaks = calcPeaks(rightData);
+
+  const peaks = _.map(_.zip(leftPeaks, rightPeaks), _.mean);
   const max = _.max(peaks);
 
   return { max, peaks };
