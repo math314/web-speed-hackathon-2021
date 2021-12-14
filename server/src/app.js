f7b0cf54c6ce3cac8885842ac1cf8ec144b430de
314@live.jp <__math>
2021-12-14 23:54:12 +0900

    compress

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,36 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
-import Compression from 'compression';
+import compression from 'compression';
 import session from 'express-session';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
 
-const app = Express(Compression());
+const app = Express();
+
+app.use(compression({
+  threshold: 0,
+  level: 9,
+  memLevel: 9,
+  filter: shouldCompress,
+}));
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+  if (req.path.startsWith("/api/")) {
+    return false;
+  }
+  if (req.path.startsWith("/fonts/")) {
+    return false;
+  }
+
+  // console.log(`${req.path}`);
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 
 app.set('trust proxy', true);
 
@@ -21,13 +45,13 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
 
-app.use((_req, res, next) => {
-  res.header({
-    'Cache-Control': 'max-age=0, no-transform',
-    Connection: 'close',
-  });
-  return next();
-});
+// app.use((_req, res, next) => {
+//   res.header({
+//     'Cache-Control': 'max-age=0, no-transform',
+//     Connection: 'close',
+//   });
+//   return next();
+// });
 
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
